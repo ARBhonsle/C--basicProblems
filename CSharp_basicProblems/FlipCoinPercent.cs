de9f13fd64885bc8a29da151e95dd9fce20bec59
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-23 09:10:13 +0530

    [aditi] Add: Flip Coin Percent is added

--- CSharp_basicProblems/FlipCoinPercent.cs
+++ CSharp_basicProblems/FlipCoinPercent.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace CSharp_basicProblems
 {
-    class Class1
+    class FlipCoinPercent
     {
+        Double[] flipCoinResult = new Double[2];
+        public FlipCoinPercent(Double[] flipCoinResult)
+        {
+            for(int i = 0; i < flipCoinResult.Length; i++)
+            {
+                this.flipCoinResult[i] = flipCoinResult[i];
+            }
+        }
+
+        public void FlipPercent()
+        {
+            Console.WriteLine("Percent of Heads: " + (flipCoinResult[1] / 10));
+            Console.WriteLine("Percent of Tails: " + (flipCoinResult[0] / 10));
+        }
+
     }
 }
--- CSharp_basicProblems/Program.cs
+++ CSharp_basicProblems/Program.cs
@@ -7,6 +7,22 @@ namespace CSharp_basicProblems
         static void Main(string[] args)
         {
             Console.WriteLine("C# Basic Problems");
+            Double[] flipResult = new Double[2];
+            Console.WriteLine("Coin Flipped 10 times");
+            Random random = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                if (random.NextDouble() < 0.5)
+                {
+                    flipResult[0]++; //Tails
+                }
+                else
+                {
+                    flipResult[1]++; //Heads
+                }
+            }  
+            FlipCoinPercent flip = new FlipCoinPercent(flipResult);
+            flip.FlipPercent();
         }
     }
 }
