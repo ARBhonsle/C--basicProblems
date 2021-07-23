75140960b17547657513e9cd749fd9547e688e56
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-23 21:16:33 +0530

    [aditi] Add:Leap Year check added

---
+++ CSharp_basicProblems/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_basicProblems
+{
+    class LeapYear
+    {
+        readonly int year;
+        public LeapYear(int year)
+        {
+            this.year = year;
+        }
+        public void CheckLeap()
+        {
+            if (year / 1000 > 0)
+            {
+                if ( (year % 4 == 0) && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0) ) )
+                {
+                    Console.WriteLine("Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("Year not Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Given year is not 4 digit");
+            }
+        }
+    }
+}
--- CSharp_basicProblems/Program.cs
+++ CSharp_basicProblems/Program.cs
@@ -7,22 +7,40 @@ namespace CSharp_basicProblems
         static void Main(string[] args)
         {
             Console.WriteLine("C# Basic Problems");
-            Double[] flipResult = new Double[2];
-            Console.WriteLine("Coin Flipped 10 times");
-            Random random = new Random();
-            for(int i = 0; i < 10; i++)
+            Console.WriteLine("Choose Problems: 1.Flip Coin Percent 2.Leap Year");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
             {
-                if (random.NextDouble() < 0.5)
-                {
-                    flipResult[0]++; //Tails
-                }
-                else
-                {
-                    flipResult[1]++; //Heads
-                }
-            }  
-            FlipCoinPercent flip = new FlipCoinPercent(flipResult);
-            flip.FlipPercent();
+                case 1:
+                    Double[] flipResult = new Double[2];
+                    Console.WriteLine("Coin Flipped 10 times");
+                    Random random = new Random();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (random.NextDouble() < 0.5)
+                        {
+                            flipResult[0]++; //Tails
+                        }
+                        else
+                        {
+                            flipResult[1]++; //Heads
+                        }
+                    }
+                    FlipCoinPercent flip = new FlipCoinPercent(flipResult);
+                    flip.FlipPercent();
+                    break;
+                case 2:
+                    Console.WriteLine("Leap Year Problem");
+                    Console.WriteLine("Input a year:");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    LeapYear leapYear = new LeapYear(year);
+                    leapYear.CheckLeap();
+                    break;
+                default: 
+                    Console.WriteLine("Wrong Input!!");
+                    break;
+            }
+            
         }
     }
 }
